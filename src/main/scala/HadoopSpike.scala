ca52e405ae72bee1bff1a73978938d9abf7d2912
Piyush Mishra <piyushmishra889@gmail.com>
2015-04-09 18:17:41 +0530

    wordcount

--- src/main/scala/HadoopSpike.scala
+++ src/main/scala/HadoopSpike.scala
@@ -1,3 +1,4 @@
+
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.IntWritable
@@ -9,38 +10,45 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 import org.apache.hadoop.util.GenericOptionsParser
 import scala.collection.JavaConversions._
+import java.util.StringTokenizer
 
-// This class performs the map operation, translating raw input into the key-value
-// pairs we will feed into our reduce operation.
-class TokenizerMapper extends Mapper[Object,Text,Text,IntWritable] {
+/**
+ * @author piyushm
+ */
+
+class TokenizerMapper
+  extends Mapper[Object, Text, Text, IntWritable] {
+
   val one = new IntWritable(1)
-  val word = new Text
-  
-  override
-  def map(key:Object, value:Text, context:Mapper[Object,Text,Text,IntWritable]#Context) = {
-    for (t <-  value.toString().split("\\s")) {
-      word.set(t)
+  val word = new Text();
+
+  override def map(key: Object, value: Text, context: Mapper[Object, Text, Text, IntWritable]#Context) = {
+    val itr = new StringTokenizer(value.toString())
+    while (itr.hasMoreTokens()) {
+      word.set(itr.nextToken())
       context.write(word, one)
     }
   }
 }
-  
-// This class performs the reduce operation, iterating over the key-value pairs
-// produced by our map operation to produce a result. In this case we just
-// calculate a simple total for each word seen.
-class IntSumReducer extends Reducer[Text,IntWritable,Text,IntWritable] {
-  override
-  def reduce(key:Text, values:java.lang.Iterable[IntWritable], context:Reducer[Text,IntWritable,Text,IntWritable]#Context) = {
-    val sum = values.foldLeft(0) { (t,i) => t + i.get }
-    context.write(key, new IntWritable(sum))
+
+class IntSumReducer
+  extends Reducer[Text, IntWritable, Text, IntWritable] {
+  private val result = new IntWritable()
+
+  override def reduce(key: Text, values: java.lang.Iterable[IntWritable],
+                      context: Reducer[Text, IntWritable, Text, IntWritable]#Context) {
+    var sum = 0;
+    for (v <- values) {
+      sum += v.get()
+    }
+    result.set(sum)
+    context.write(key, result)
   }
 }
-  
-// This class configures and runs the job with the map and reduce classes we've
-// specified above.
+
 object WordCount {
 
-  def main(args:Array[String]):Int = {
+  def main(args: Array[String]): Int = {
     val conf = new Configuration()
     val otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs
     if (otherArgs.length != 2) {
@@ -58,5 +66,4 @@ object WordCount {
     FileOutputFormat.setOutputPath(job, new Path((args(1))))
     if (job.waitForCompletion(true)) 0 else 1
   }
-
 }
