b6d78092bf88dddc8b1ebe0e91c11202fbed3f06
Piyush Mishra <piyushmishra889@gmail.com>
2015-04-09 17:43:53 +0530

    wordcount

--- src/main/scala/HadoopSpike.scala
+++ src/main/scala/HadoopSpike.scala
@@ -1,66 +1,62 @@
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.IntWritable
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.mapreduce.Job
+import org.apache.hadoop.mapreduce.Mapper
+import org.apache.hadoop.mapreduce.Reducer
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
+import org.apache.hadoop.util.GenericOptionsParser
+import scala.collection.JavaConversions._
 
-import java.io.IOException;
-import java.util.StringTokenizer;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
-/**
- * @author piyushm
- */
-
-class WordCount {
-
-  class TokenizerMapper
-    extends Mapper[Object, Text, Text, IntWritable] {
-
-    private val one = new IntWritable(1)
-    private val word = new Text();
-
-    def map(key: Object, value: Text, context: Context) = {
-      val itr = new StringTokenizer(value.toString())
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken())
-        context.write(word, one)
-      }
+// This class performs the map operation, translating raw input into the key-value
+// pairs we will feed into our reduce operation.
+class TokenizerMapper extends Mapper[Object,Text,Text,IntWritable] {
+  val one = new IntWritable(1)
+  val word = new Text
+  
+  override
+  def map(key:Object, value:Text, context:Mapper[Object,Text,Text,IntWritable]#Context) = {
+    for (t <-  value.toString().split("\\s")) {
+      word.set(t)
+      context.write(word, one)
     }
   }
-
-  class IntSumReducer
-    extends Reducer[Text, IntWritable, Text, IntWritable] {
-    private val result = new IntWritable()
-
-    def reduce(key: Text, values: Iterable[IntWritable],
-               context: Context) {
-      var sum = 0;
-      for (v <- values) {
-        sum += v.get()
-      }
-      result.set(sum)
-      context.write(key, result)
-    }
+}
+  
+// This class performs the reduce operation, iterating over the key-value pairs
+// produced by our map operation to produce a result. In this case we just
+// calculate a simple total for each word seen.
+class IntSumReducer extends Reducer[Text,IntWritable,Text,IntWritable] {
+  override
+  def reduce(key:Text, values:java.lang.Iterable[IntWritable], context:Reducer[Text,IntWritable,Text,IntWritable]#Context) = {
+    val sum = values.foldLeft(0) { (t,i) => t + i.get }
+    context.write(key, new IntWritable(sum))
   }
+}
+  
+// This class configures and runs the job with the map and reduce classes we've
+// specified above.
+object WordCount {
 
-  def main(args: Array[String]) {    
-    val conf = new Configuration();
-    val job = Job.getInstance(conf, "word count");
-    job.setJarByClass(classOf[WordCount]);
+  def main(args:Array[String]):Int = {
+    val conf = new Configuration()
+    val otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs
+    if (otherArgs.length != 2) {
+      println("Usage: wordcount <in> <out>")
+      return 2
+    }
+    val job = new Job(conf, "word count")
+    job.setJarByClass(classOf[TokenizerMapper])
     job.setMapperClass(classOf[TokenizerMapper])
     job.setCombinerClass(classOf[IntSumReducer])
     job.setReducerClass(classOf[IntSumReducer])
     job.setOutputKeyClass(classOf[Text])
     job.setOutputValueClass(classOf[IntWritable])
-    println(args(0), args(2))
     FileInputFormat.addInputPath(job, new Path(args(0)))
-    FileOutputFormat.setOutputPath(job, new Path(args(1)))
-    val value = if (job.waitForCompletion(true)) 0 else 1
-    System.exit(value)
+    FileOutputFormat.setOutputPath(job, new Path((args(1))))
+    if (job.waitForCompletion(true)) 0 else 1
   }
+
 }
