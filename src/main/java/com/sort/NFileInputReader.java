75e60640999f3a4825d468f207fe40139c3ee879
Piyush Mishra <piyushmishra889@gmail.com>
2015-04-15 19:13:55 +0530

    NLine input

--- src/main/java/com/sort/NFileInputReader.java
+++ src/main/java/com/sort/NFileInputReader.java
@@ -1,22 +1,104 @@
 package com.sort;
 
-public class NFileInputReader {
-	
-	
-	public static class NLineFileFormat {
-		
-		
-	}
-	
-	
-	public static class NLineRecordReader {
-		
-		
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class NFileInputReader extends Configured implements Tool {
+
+	public static class TokenizerMapper extends
+			Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
 	}
 
-	public static class NLineInputFormat {
-		
-		
+	public static class IntSumReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
 	}
-	
+
+	public static class Combiner extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Configuration conf = getConf();
+		conf.set("mapreduce.input.lineinputformat.linespermap", "3");
+		Job job = Job.getInstance(conf);
+		job.setJobName("CustomFileReader");
+		job.setJarByClass(NFileInputReader.class);
+
+		// set the InputFormat of the job to our InputFormat
+		job.setInputFormatClass(NLineInputFormat.class);
+
+		// the keys are words (strings)
+		job.setOutputKeyClass(Text.class);
+		// the values are counts (ints)
+		job.setOutputValueClass(IntWritable.class);
+
+		// use the defined mapper
+		job.setMapperClass(TokenizerMapper.class);
+		// use the WordCount Reducer
+		job.setReducerClass(IntSumReducer.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+
+	public static void main(String args[]) throws Exception {
+		Configuration conf = new Configuration();
+		int code = ToolRunner.run(conf, new NFileInputReader(), args);
+		System.exit(code);
+	}
+
 }
